57af16db7eac1e80c50019e0e532c61d79fd8cab
AntonioRosano <antoniorosano.02@gmail.com>
2026-03-04 17:04:11 +0100

    modifica di projectUtils

--- src/projectUtils.py
+++ src/projectUtils.py
@@ -5,24 +5,24 @@ from map import Map, CellType
 import random
 
 class Pose:
-    def __init__(self, x_, y_):
-        self.x = x_
-        self.y = y_
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
     
-    def dist(self, x_, y_):
-        return math.dist([self.x, self.y], [x_, y_])
+    def dist(self, x, y):
+        return math.dist([self.x, self.y], [x, y])
 
-    def FindNear(self, targets):
+    def FindNear(self, targets: list[Pose]) -> Pose | None:
         if not targets: return None 
         return min(targets, key=lambda t: self.dist(t.x, t.y))
 
 class Visibility:
-    def __init__(self, reach_, radius_, xRays_=False):
-        self.reach = reach_
-        self.radius = radius_
-        self.xRays = xRays_
+    def __init__(self, reach, radius, xRays=False):
+        self.reach = reach
+        self.radius = radius
+        self.xRays = xRays
     
-    def update(self, position, localMap, grid):
+    def update(self, position, localMap : Map, grid : np.matrix):
         rows, cols = grid.shape
         visited = {(position.x, position.y)}
         queue = deque([(position.x, position.y, 0)])
@@ -48,11 +48,11 @@ class Visibility:
                         queue.append((nr, nc, deep + 1))
 
 class Strategy:
-    def __init__(self, storage_x_, storage_y_, max_energy_, num_goal_, epsilon=0.8):
-        self.storage = [Pose(storage_x_, storage_y_)]
-        self.max_energy = max_energy_
-        self.current_energy = max_energy_
-        self.num_goal = num_goal_
+    def __init__(self, storage_x : int, storage_y : int, max_energy : int, num_goal : int, epsilon=0.8):
+        self.storage = [Pose(storage_x, storage_y)]
+        self.max_energy = max_energy
+        self.current_energy = max_energy
+        self.num_goal = num_goal
         self.status = 1
         self.epsilon = epsilon 
 
@@ -60,7 +60,7 @@ class Strategy:
     def _get_random_move(self): 
         return random.choice([(-1,0), (1,0), (0,-1), (0,1)])
 
-    def next_move(self, position, localMap):
+    def next_move(self, position : Pose, localMap : Map):
         if self.status == 0: return None
 
         if self.status == 1: # Esplorazione  
@@ -78,14 +78,14 @@ class Strategy:
         if self.current_energy <= 0: self.status = 0
 
 class Agente:
-    def __init__(self, position_, visibility_, charge_, localMap_, strategy_):
+    def __init__(self, position_ : Pose, visibility_ : Visibility, charge_ : int, localMap_: Map, strategy_: Strategy):
         self.position = position_
         self.visibility = visibility_
         self.charge = charge_
         self.localMap = localMap_
         self.strategy = strategy_
 
-    def action(self, Map): 
+    def action(self, Map : Map): 
         self.visibility.update(self.position, self.localMap, Map.grid)
         res = self.strategy.next_move(self.position, self.localMap)
         if res is None: return
